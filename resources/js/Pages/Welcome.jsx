c4254133afee519f6e422a1dfdb483bf44dd4306
mzidanrifqirtkj <mzidanrifqirtkj@gmail.com>
2024-09-04 10:56:31 +0700

    edit rebase

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -48,6 +48,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                             className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                         >
                                             Login 2
+                                            edit rebase
                                         </Link>
                                         <Link
                                             href={route('register')}
